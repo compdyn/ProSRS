b7e4fbf0fa05ba0a49696c9d16612ff403f7ef37
Chenchao Shou <chenchao@Chenchaos-MacBook-Pro.local>
2019-03-03 14:37:13 -0600

    create a separate file for release version control

---
+++ prosrs/version.py
@@ -0,0 +1,9 @@
+"""
+Copyright (C) 2019 Chenchao Shou
+
+Licensed under Illinois Open Source License (see the file LICENSE). For more information
+about the license, see http://otm.illinois.edu/disclose-protect/illinois-open-source-license.
+
+Define release version.
+"""
+__version__ = '0.1.1'
